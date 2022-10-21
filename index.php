2231447025be4db10a70bf20184715170814789f
Araceli Cabello <81246309+AraceliCabello18@users.noreply.github.com>
2022-10-20 19:08:55 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include "header.php"; ?>
+    <!-- Page Content -->
+    <div class="container">
+    <div class="jumbotron">
+			<h1 class="display-4">Los Ajolotitos</h1>
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<img src="librerias/img/Ajolotitos .png" class="img-fluid" alt="Responsive image">
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<table class="table table-hover">
+								<tr>
+									<th>
+                                    Tiendas de ropa vintage y de segunda mano en la CDMX
+									</th>
+								</tr>
+								<tr>
+									<th>
+                                    Esta tienda prendas y accesorios en buen estado y a presios accecibles
+									</th>
+								</tr>
+								<tr>
+									<th>
+									La moda da es un apoyo visual y de actitud que eliges para reafirmar y complementar tu identidad
+									</th>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<hr class="my-4">
+			<p>
+            Los Ajolotitos Tecnológicos S.A De C.V
+			</p>
+		</div>
+    </div>
+    <?php include "footer.php"; ?>
